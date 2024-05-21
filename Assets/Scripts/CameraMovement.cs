2be56c78855108b2f013580afc17be8f468f9ff6
Phoque <lafont_jordan@hotmail.fr>
2024-05-21 22:45:14 +0200

    moving camera between screens

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,48 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+	[SerializeField] CinemachineVirtualCamera camShop;
+	[SerializeField] CinemachineVirtualCamera camWorkshop;
+	[SerializeField] CinemachineVirtualCamera camGraveyard;
+
+	private void Start()
+	{
+		FocusOnWorkshop();
+	}
+
+	private void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.Alpha1)) FocusOnShop();
+		if(Input.GetKeyDown(KeyCode.Alpha2)) FocusOnWorkshop();
+		if(Input.GetKeyDown(KeyCode.Alpha3)) FocusOnGraveyard();
+	}
+
+	void UnfocusCameras()
+	{
+		camShop.Priority = 0;
+		camWorkshop.Priority = 0;
+		camGraveyard.Priority = 0;
+	}
+
+	public void FocusOnShop()
+	{
+		UnfocusCameras();
+		camShop.Priority = 10;
+	}
+
+	public void FocusOnWorkshop()
+	{
+		UnfocusCameras();
+		camWorkshop.Priority = 10;
+	}
+
+	public void FocusOnGraveyard()
+	{
+		UnfocusCameras();
+		camGraveyard.Priority = 10;
+	}
+}
